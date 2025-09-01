af5a8d39785abd032544adc693f3af3d5bb6d09b
erdospeet11 <erdospepe9@gmail.com>
2025-09-01 21:13:56 +0200

    Updated implementation

---
+++ engine/engine.cpp
@@ -0,0 +1,21 @@
+#include "engine.hpp"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+Engine::Engine()
+{
+}
+
+Engine::~Engine()
+{
+}
+
+void Engine::run()
+{
+}
