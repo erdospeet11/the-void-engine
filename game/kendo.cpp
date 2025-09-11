ec6ffb4826ed11573632d10c1386b2044514c43c
erdospeet11 <erdospepe9@gmail.com>
2025-09-11 11:07:46 +0200

    Added window class

--- game/kendo.cpp
+++ game/kendo.cpp
@@ -1,10 +1,12 @@
 #include "Engine.hpp"
+#include "Window.hpp"
 #include <stdio.h>
 #include <iostream>
 
 int main(){
     Engine engine;
-
+    Window window("Kendo", 800, 600);
+    
     try{
         engine.run();
     } catch (int errorCode){
