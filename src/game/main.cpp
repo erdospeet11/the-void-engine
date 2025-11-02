7275074ad48b2cc6616a600ea41e30d4ca2dddbd
erdospeet11 <erdospepe9@gmail.com>
2025-11-02 09:37:49 +0100

    Integrate Assimp for model loading in main.cpp and remove obsolete model file Wizardus Maximus.glb

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -2,6 +2,10 @@
 #include "../engine/Engine.hpp"
 #include "../engine/Mesh.hpp"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 int main(int argc, char* args[])
 {
     //VoidEngine::Engine engine;
@@ -9,6 +13,16 @@ int main(int argc, char* args[])
 
     // Create window with OpenGL context
     VoidEngine::Window window("void", 640*2, 480*2);
+
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile("../src/game/assets/models/wizard.glb", aiProcess_Triangulate | aiProcess_FlipUVs);
+    if (!scene) {
+        std::cerr << "Error: " << importer.GetErrorString() << std::endl;
+        return 1;
+    }
+    const aiMesh* mesh = scene->mMeshes[0];
+    std::cout << "Mesh: " << mesh << std::endl;
+    std::cout << "Mesh: " << mesh->mName.C_Str() << std::endl;
 
     // Main loop
     while (!window.shouldClose()) {
